90c511d10c1675c1e4c29ecaa8677ead91adaf46
Tanmay Choudhary <tanmay2001choudhary@gmail.com>
2024-06-24 21:44:43 +0530

    initial commit

---
+++ routes/ownersRouter.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express()
+const ownerModel = require('../models/owner-model')
+
+if (process.env.NODE_ENV === 'development') {
+  router.post('/create', async (req, res) => {
+    let owners = await ownerModel.find()
+    if (owners.length > 0) {
+      return res
+        .status(500)
+        .send("You don't have permission to create a new owner")
+    }
+    let { fullname, email, password } = req.body
+    let createdOwner = await ownerModel.create({
+      fullname,
+      email,
+      password,
+    })
+    res.status(200).send(createdOwner)
+  })
+}
+
+router.get('/', (req, res) => {
+  res.send("hey it's working")
+})
+
+module.exports = router
